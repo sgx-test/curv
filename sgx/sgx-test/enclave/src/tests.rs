e24ce32de9fa1003f3a469a0fc516f614c3abe1c
prz23 <panruizheng@zju.edu.cn>
2021-07-29 17:32:12 +0800

    add sgx env

---
+++ sgx/sgx-test/enclave/src/tests.rs
@@ -0,0 +1,11 @@
+use curv::*;
+use curv::ErrorKey;
+
+use std::prelude::v1::*;
+use std::panic::{catch_unwind, resume_unwind, AssertUnwindSafe};
+
+//#[test]
+pub fn insert_get_remove_one() {
+
+}
+
