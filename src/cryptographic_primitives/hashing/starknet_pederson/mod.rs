39b5d3ded225d2091dfa2f44d330845132cca567
Ljiacheng <Ljiachenggh@gmail.com>
2023-06-01 11:25:03 +0800

    update hash

--- src/cryptographic_primitives/hashing/starknet_pederson/mod.rs
+++ src/cryptographic_primitives/hashing/starknet_pederson/mod.rs
@@ -5,20 +5,10 @@ use starknet_ff::FieldElement;
 pub use pederson_hash::*;
 
 pub fn hash_util(message: &[FieldElement]) -> FieldElement {
-    let mut hash = FieldElement::from_bytes_be(&[0u8; 32]).unwrap();
+    let mut hash = FieldElement::ZERO;
     for x in message {
-        hash = pedersen_hash(x, &hash);
+        hash = pedersen_hash(&hash, x);
     }
+    hash = pedersen_hash(&hash, &FieldElement::from(message.len()));
     hash
-}
-
-#[test]
-fn test_hash_util() {
-    let mut message = vec![];
-    message.push(FieldElement::from_bytes_be(&[1u8; 32]).unwrap());
-    message.push(FieldElement::from_bytes_be(&[2u8; 32]).unwrap());
-    message.push(FieldElement::from_bytes_be(&[3u8; 32]).unwrap());
-    message.push(FieldElement::from_bytes_be(&[4u8; 32]).unwrap());
-
-    assert_eq!(hex::encode(hash_util(&message).to_bytes_be()), "07e25573fbbd80e68b812e1e983f2b02b82dedb73746f75da20ea7b20c014e5f");
 }
